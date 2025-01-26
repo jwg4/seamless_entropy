516b7723f4c36967dd52765f4ed1315d4790be13
Jack Grahl <jack.grahl@gmail.com>
2025-01-26 18:57:31 +0000

    Change name of function.

--- test/test_config.py
+++ test/test_config.py
@@ -1,8 +1,8 @@
 from os import environ
 
-from seamless_entropy import entropy
+from seamless_entropy import binary_entropy
 
 
 def test_function_details():
     target = environ["SEAMLESS_TEST_TARGET_PLATFORM"]
-    assert entropy._platform == target
+    assert binary_entropy._platform == target
